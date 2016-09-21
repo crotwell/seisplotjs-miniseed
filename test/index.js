aa6f5668ea01d0d60a0369e3822701a32664c1b8
Philip Crotwell <crotwell@seis.sc.edu>
2016-09-21 10:16:02 -0400

    add a test, using qunit

---
+++ test/index.js
@@ -0,0 +1,15 @@
+
+var fs = require('fs');
+
+test("load miniseed file", function () {
+    ok(true, "this test is fine");
+    
+    var mseedData = fs.readFileSync('test/CO_JSC.seed');
+    equal(7168, mseedData.length, "file size, data size");
+    var parsed = parseDataRecords(mseedData.buffer);
+    equal(14, parsed.length, "14 data records");
+    equal("JSC", parsed[0].header.staCode, "Station is JSC");
+    var decomp = parsed[0].decompress();
+    ok(true, "data decompressed without error");
+});
+
