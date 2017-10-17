b9f8a9a5f6e7e5e635097395077263f27882d537
Philip Crotwell <crotwell@seis.sc.edu>
2017-10-17 11:48:34 -0400

    update to new qunit

--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
 
 var fs = require('fs');
 
-test("load miniseed file", function () {
-    ok(true, "this test is fine");
-    
-    var mseedData = fs.readFileSync('test/CO_JSC.mseed');
-    equal(7168, mseedData.length, "file size, data size");
-    var parsed = parseDataRecords(mseedData.buffer);
-    equal(14, parsed.length, "14 data records");
-    equal("JSC", parsed[0].header.staCode, "Station is JSC");
-    var decomp = parsed[0].decompress();
-    ok(true, "data decompressed without error");
-});
+QUnit.test("load miniseed file", function (assert) {
+    assert.ok(true, "this test is fine");
 
+    var mseedData = fs.readFileSync('test/CO_JSC.mseed');
+    assert.equal(7168, mseedData.length, "file size, data size");
+    var parsed = parseDataRecords(mseedData.buffer);
+    assert.equal(14, parsed.length, "14 data records");
+    assert.equal("JSC", parsed[0].header.staCode, "Station is JSC");
+    var decomp = parsed[0].decompress();
+    assert.ok(true, "data decompressed without error");
+});
