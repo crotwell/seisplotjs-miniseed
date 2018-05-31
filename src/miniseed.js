a85f013b5d77c12502a21e218589ceaa9f4cfb56
Philip Crotwell <crotwell@seis.sc.edu>
2018-05-31 13:59:40 -0400

    add flow and related bug fixes

--- src/miniseed.js
+++ src/miniseed.js
@@ -1,3 +1,5 @@
+// @flow
+
 /*global DataView*/
 /**
  * Philip Crotwell
@@ -5,14 +7,18 @@
  * http://www.seis.sc.edu
  */
 
+// special due to flow
+import {hasArgs, hasNoArgs, isStringArg, isNumArg, checkStringOrDate, stringify} from 'seisplotjs-model';
+
 import * as seedcodec from 'seisplotjs-seedcodec';
 import * as model from 'seisplotjs-model';
+const moment = model.moment;
 
 /* re-export */
 export { seedcodec, model };
 
 /** parse arrayBuffer into an array of DataRecords. */
-export function parseDataRecords(arrayBuffer) {
+export function parseDataRecords(arrayBuffer: ArrayBuffer) {
   let dataRecords = [];
   let offset = 0;
   while (offset < arrayBuffer.byteLength) {
@@ -28,9 +34,13 @@ export function parseDataRecords(arrayBuffer) {
   * Currently only blockette 1000 is parsed, others are separated,
   * but left as just a DataView. */
 export class DataRecord {
-  constructor(dataView) {
+  header: DataHeader;
+  length: number;
+  data: DataView;
+  decompData: Array<number> | void;
+  constructor(dataView: DataView) {
     this.header = new DataHeader(dataView);
-    this.length = this.header.numSamples;
+  //  this.length = this.header.numSamples;
 
     this.data = new DataView(dataView.buffer,
                              dataView.byteOffset+this.header.dataOffset,
@@ -52,8 +62,32 @@ export class DataRecord {
 }
 
 export class DataHeader {
-
-  constructor(dataView) {
+  seq: string;
+  typeCode: number;
+  continuationCode: number;
+  staCode: string;
+  locCode: string;
+  chanCode: string;
+  netCode: string;
+  startBTime: BTime;
+  numSamples: number;
+  encoding: number;
+  littleEndian: boolean;
+  sampRateFac: number;
+  sampRateMul: number;
+  sampleRate: number;
+  activityFlags: number;
+  ioClockFlags: number;
+  dataQualityFlags: number;
+  numBlockettes: number;
+  timeCorrection: number;
+  dataOffset: number;
+  blocketteOffset: number;
+  recordSize: number;
+  blocketteList: Array<Blockette>;
+  start: moment;
+  end: moment;
+  constructor(dataView: DataView) {
     this.seq = makeString(dataView, 0, 6);
     this.typeCode = dataView.getUint8(6);
     this.continuationCode = dataView.getUint8(7);
@@ -88,17 +122,15 @@ export class DataHeader {
       if (nextOffset == 0) {
         nextOffset = offset; // zero length, probably an error...
       }
-      let blockette = new Blockette(dataView, offset, nextOffset-offset);
+      let blockette = new Blockette(dataView, offset, nextOffset-offset, headerByteSwap);
+      if (blockette.type === 1000) {
+        blockette = new Blockette1000(dataView, offset, nextOffset-offset, headerByteSwap);
+      }
       this.blocketteList.push(blockette);
       offset = nextOffset;
-      if (blockette.type == 1000) {
+      if (blockette instanceof Blockette1000) {
         this.recordSize = 1 << blockette.dataRecordLengthByte;
         this.encoding = blockette.encoding;
-        if (blockette.wordOrder == 0) {
-          this.swapBytes = true;
-        } else {
-          this.swapBytes = false;
-        }
         this.littleEndian = (blockette.wordOrder === 0);
       }
     }
@@ -111,7 +143,7 @@ export class DataHeader {
     return this.netCode+"."+this.staCode+"."+this.locCode+"."+this.chanCode+" "+this.start.toISOString()+" "+this.encoding;
   }
 
-  calcSampleRate() {
+  calcSampleRate() :number {
     let factor = this.sampRateFac;
     let multiplier = this.sampRateMul;
     let sampleRate = 10000.0; // default (impossible) value;
@@ -124,15 +156,29 @@ export class DataHeader {
     return sampleRate;
   }
 
-  timeOfSample(i) {
-    return new Date(this.start.getTime() + 1000*i/this.sampleRate);
+  timeOfSample(i: number): moment {
+    return moment.utc(this.start.getTime()).add(1000*i/this.sampleRate, 'second');
   }
 }
 
 export class Blockette {
-  constructor(dataView, offset, length, headerByteSwap) {
+  type: number;
+  body: DataView;
+
+  constructor(dataView: DataView, offset: number, length: number, headerByteSwap: boolean) {
     this.type = dataView.getUint16(offset, headerByteSwap);
     this.body = new DataView(dataView.buffer, dataView.byteOffset+offset, length);
+  }
+}
+
+
+export class Blockette1000 extends Blockette {
+  encoding: number;
+  dataRecordLengthByte: number;
+  wordOrder: number;
+  constructor(dataView: DataView, offset: number, length: number, headerByteSwap: boolean) {
+    super(dataView, offset, length, headerByteSwap);
+    if (this.type != 1000) {throw new Error("Not a blockette1000: "+this.type);}
     if (this.type == 1000) {
       this.encoding = this.body.getUint8(4);
       this.dataRecordLengthByte = this.body.getUint8(6);
@@ -153,8 +199,15 @@ function makeString(dataView, offset, length) {
 }
 
 export class BTime {
-  constructor(dataView, offset, byteSwap) {
-    if (typeof byteSwap === 'undefined') { byteSwap = false; }
+  year: number;
+  jday: number;
+  hour: number;
+  min: number;
+  sec: number;
+  tenthMilli: number;
+  length: number;
+  constructor(dataView: DataView, offset:number, byteSwap:?boolean) {
+    if ( ! byteSwap ) { byteSwap = false; }
     this.length = 10;
     this.year = dataView.getInt16(offset, byteSwap);
     this.jday = dataView.getInt16(offset+2, byteSwap);
@@ -164,21 +217,21 @@ export class BTime {
     // byte 7 unused, alignment
     this.tenthMilli = dataView.getInt16(offset+8, byteSwap);
   }
-  toString() {
-    return this.year+"-"+this.jday+" "+this.hour+":"+this.min+":"+this.sec+"."+this.tenthMilli+" "+this.getDate().toISOString();
+  toString(): string {
+    return this.year+"-"+this.jday+" "+this.hour+":"+this.min+":"+this.sec+"."+this.tenthMilli+" "+this.toDate().toISOString();
   }
 
-  toDate() {
-    return new Date(Date.UTC(this.year, 0, this.jday, this.hour, this.min, this.sec, this.tenthMilli/10));
+  toDate(): moment {
+    return new moment.utc(this.year, 0, this.jday, this.hour, this.min, this.sec, this.tenthMilli/10);
   }
 }
 
 
-function checkByteSwap(bTime) {
+function checkByteSwap(bTime): boolean {
   return bTime.year < 1960 || bTime.year > 2055;
 }
 
-export function areContiguous(dr1, dr2) {
+export function areContiguous(dr1: DataRecord, dr2: DataRecord) {
     let h1 = dr1.header;
     let h2 = dr2.header;
     return h1.end.getTime() < h2.start.getTime()
@@ -188,7 +241,7 @@ export function areContiguous(dr1, dr2) {
 /** concatentates a sequence of DataRecords into a single seismogram object.
   * Assumes that they are all contiguous and in order. Header values from the first
   * DataRecord are used. */
-export function createSeismogram(contig) {
+export function createSeismogram(contig: Array<DataRecord>): model.Seismogram {
   let y = [];
   for (let i=0; i<contig.length; i++) {
     y = y.concat(contig[i].decompress());
@@ -213,7 +266,7 @@ export function createSeismogram(contig) {
  * This assumes all data records are from the same channel, byChannel
  * can be used first if multiple channels may be present.
  */
-export function merge(drList) {
+export function merge(drList: Array<DataRecord>): Array<model.Seismogram> {
   let out = [];
   let currDR;
   drList.sort(function(a,b) {
@@ -241,9 +294,9 @@ export function merge(drList) {
 }
 
 
-export function segmentMinMax(segment, minMaxAccumulator) {
+export function segmentMinMax(segment: model.Seismogram, minMaxAccumulator:? Array<number>) :Array<number> {
   if ( ! segment.y()) {
-    throw new Error("Segment does not have a y field, doesn't look like a seismogram segment. "+Array.isArray(segment)+" "+segment);
+    throw new Error("Segment does not have a y field, doesn't look like a seismogram segment. "+stringify(segment));
   }
   let minAmp = Number.MAX_SAFE_INTEGER;
   let maxAmp = -1 * (minAmp);
@@ -251,12 +304,13 @@ export function segmentMinMax(segment, minMaxAccumulator) {
     minAmp = minMaxAccumulator[0];
     maxAmp = minMaxAccumulator[1];
   }
-  for (let n = 0; n < segment.y().length; n++) {
-    if (minAmp > segment.y()[n]) {
-      minAmp = segment.y()[n];
+  let yData: Array<number> = segment.y();
+  for (let n = 0; n < yData.length; n++) {
+    if (minAmp > yData[n]) {
+      minAmp = yData[n];
     }
-    if (maxAmp < segment.y()[n]) {
-      maxAmp = segment.y()[n];
+    if (maxAmp < yData[n]) {
+      maxAmp = yData[n];
     }
   }
   return [ minAmp, maxAmp ];
@@ -264,16 +318,18 @@ export function segmentMinMax(segment, minMaxAccumulator) {
 
 /** splits a list of data records by channel code, returning a Map
   * with each NSLC string mapped to an array of data records. */
-export function byChannel(drList) {
+export function byChannel(drList: Array<DataRecord>): Map<string, Array<DataRecord>> {
   let out = new Map();
   let key;
   for (let i=0; i<drList.length; i++) {
     let currDR = drList[i];
     key = currDR.codes();
-    if (! out.has(key)) {
-      out.set(key, [ currDR ]);
+    let array = out.get(key);
+    if ( ! array) {
+      array = [currDR];
+      out.set(key, array);
     } else {
-      out.get(key).push(currDR);
+      array.push(currDR);
     }
   }
   return out;
